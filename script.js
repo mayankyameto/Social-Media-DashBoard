c098ca6e289c25d1a5bc4beed2635fbb23d214df
Mayank <124571236+mayankyameto@users.noreply.github.com>
2024-02-11 00:38:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let btn  = document.querySelector("input[name=theme]");
+
+
+btn.addEventListener("change", function(){
+    if (this.checked) {
+        trans();
+        document.documentElement.setAttribute('data-theme','dark');
+        
+    }
+    else{
+        trans();
+        document.documentElement.setAttribute('data-theme','light');
+    }
+})
+   
+let trans = () => {
+    document.documentElement.classList.add('transition');
+
+    window.setTimeout(() =>{
+        // document.documentElement.classList.remove('transition');
+    },1000)
+}
